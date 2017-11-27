4abaa391375d5431ea0efc6696803154106e9f67
earjkam <arjun.kamath@ericsson.com>
2017-11-27 12:17:48 +0200

    values look random now

--- server/generate_values.js
+++ server/generate_values.js
@@ -1,11 +1,11 @@
 var json = {}
 var start_epoch = 1511686472; // Mon, 26 Nov 2017 08:54:32 GMT
-var increment = 100; // one minute
+var increment = 3600; // 5 minutes
 var no_of_values = 5000;
 
 for (var i=0; i< no_of_values; i++) {
 
-	json[start_epoch.toString()] = Math.floor((Math.random() * 10));
+	json[start_epoch.toString()] = Math.floor((Math.random() * 9)) - 3;
 	start_epoch += increment;
 
 }
